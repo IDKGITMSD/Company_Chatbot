49132e5501e6311bef56ef90a33e9369ccacf88d
MAHIMNA DAVE <153760152+IDKGITMSD@users.noreply.github.com>
2024-09-06 12:44:59 +0530

    Update 2fa.py

--- 2fa.py
+++ 2fa.py
@@ -61,6 +61,6 @@ if st.session_state['otp_sent'] and not st.session_state['authenticated']:
 
 # Step 3: Display content for authenticated users
 if st.session_state['authenticated']:
-    st.switch_page("pages/travel_guru.py")
+    st.switch_page("pages/workassist.py")
     st.header("Welcome to the Secure Section")
     st.write("You have successfully logged in with 2FA.")
